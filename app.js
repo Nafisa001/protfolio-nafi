2eea319308f55daeadda3b2acfbbeb5af93a8716
Your Name <you@example.com>
2026-05-13 15:52:13 +0600

    update

---
+++ app.js
@@ -0,0 +1,31 @@
+const form = document.getElementById("portfolioForm");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const formData = {
+    name: form.name.value,
+    email: form.email.value,
+    Phone number: form.Phone number.value,
+    portfolio: form.portfolio.value,
+  };
+
+  try {
+    const response = await fetch("https://protfolio-backend-beta.vercel.app/api/portfolio", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await response.json();
+
+    alert("Submitted Successfully!");
+    console.log(data);
+
+  } catch (error) {
+    console.error(error);
+    alert("Submission Failed");
+  }
+});
